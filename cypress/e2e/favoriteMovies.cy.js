2a6e5acabddd462bf35db4d184ed6a89ba4d0d78
SyifanIkhtiardi <73370522+SyifanIkhtiardi@users.noreply.github.com>
2023-07-07 08:05:13 +0700

    Update login test script so it can get session cookies

--- cypress/e2e/favoriteMovies.cy.js
+++ cypress/e2e/favoriteMovies.cy.js
@@ -1,5 +1,30 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.tmdbLogin()
-  })
+describe("Favorite movie on The Movie Database", () => {
+  const username = Cypress.env("username");
+  const password = Cypress.env("password");
+  before(() => {
+    // Perform login
+    cy.tmdbLogin(username, password)
+
+    // Get cookie
+    cy.getCookies().then((cookies) => {
+      
+    // Save the cookies to be used for future requests
+    cy.writeFile("cypress/fixtures/cookies.json", JSON.stringify(cookies));
+    });
+  });
+
+  context("When marks a movie as favorite", () => {
+    beforeEach(() => {
+      // Set cookie for each test using custom command
+      cy.setLoginSession();
+    });
+
+    it("should add a movie to user's favorite movies list", () => {
+      cy.visit("https://www.themoviedb.org/u/Gompachiro")
+    });
+    it("should display movie added in the user's favorite movies section on the profile page", () => {
+      cy.visit("https://www.themoviedb.org/u/Gompachiro")
+    });
+  });
+  
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,14 +1,31 @@
-Cypress.Commands.add(("tmdbLogin"), () => {
-    cy.visit("https://www.themoviedb.org/login");
+Cypress.Commands.add(("tmdbLogin"), (username, password) => {
+    cy.visit("https://www.themoviedb.org");
+    cy.contains("Login").click();
     cy.get("#username")
-        .type(Cypress.env("username"))
-        .should('have.value', Cypress.env("username"));
+        .type(username)
+        .should('have.value', username);
     cy.get("#password")
-    .type(Cypress.env("password"), {log: false})
+    .type(password, {log: false})
     .should(el$ => {
-        if (el$.val() !== Cypress.env("password")) {
+        if (el$.val() !== password) {
           throw new Error('Different value of typed password')
         }
       });
     cy.get("#login_button").click();
+});
+
+// Set cookies command
+Cypress.Commands.add(("setLoginSession"), () => {
+  cy.fixture("cookies.json").then((cookies) => {
+    cookies.forEach((cookie) => {
+      cy.setCookie(cookie.name, cookie.value, {
+        path: cookie.path,
+        secure: cookie.secure,
+        domain: cookie.domain,
+        expiry: cookie.expiry,
+        sameSite: cookie.sameSite,
+        httpOnly: cookie.httpOnly,
+      });
+    });
+  });
 })
